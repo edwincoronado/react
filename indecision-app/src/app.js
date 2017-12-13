2287ceb6b0f4ca61435062d964fdbda07e52f363
edwin.coronado <edwin.coronado@servicenow.com>
2017-12-12 22:43:56 -0800

    First quick snippet. Needs commands: babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch and live-server public

---
+++ indecision-app/src/app.js
@@ -0,0 +1,40 @@
+// JSX - JavaScript XML
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info'
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Edwin',
+    age: 26,
+    location: 'San Diego'
+};
+
+function getLocation(location) {
+    if (location)
+        return <p> Location: {location} </p>; 
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        <p>Age: {user.age}</p>
+        {getLocation(user.location)}
+    </div>
+);
+
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
