d69382713df37234aeb6e2c20c8a39b2f997d924
edwin.coronado <edwin.coronado@servicenow.com>
2017-12-13 18:23:49 -0800

    First working version with renderers.

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,73 +1,88 @@
 'use strict';
 
-// JSX - JavaScript XML
-
 var app = {
     title: 'Indecision App',
-    subtitle: 'This is some info'
+    subtitle: 'This is some info',
+    options: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'ol',
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+var removeOptions = function removeOptions() {
+    app.options = [];
+    render();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'Item one'
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        app.options.length > 0 ? React.createElement(
+            'p',
+            null,
+            'Here are your options'
+        ) : 'No options',
+        React.createElement(
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
-            'li',
+            'button',
+            { disabled: app.options.length === 0, onClick: removeOptions },
+            'Remove Options'
+        ),
+        React.createElement(
+            'ol',
             null,
-            'Item two'
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    'Option: ',
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
-    )
-);
-
-var user = {
-    name: 'Edwin',
-    age: 26,
-    location: 'San Diego'
+    );
+    ReactDOM.render(template, appRoot);
 };
 
-function getLocation(location) {
-    if (location) return React.createElement(
-        'p',
-        null,
-        ' Location: ',
-        location,
-        ' '
-    );
-}
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
 var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+render();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,40 +1,56 @@
-// JSX - JavaScript XML
-
-var app = {
+const app = {
     title: 'Indecision App',
-    subtitle: 'This is some info'
+    subtitle: 'This is some info',
+    options: []
 };
 
-var template = (
-    <div>
-        <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-var user = {
-    name: 'Edwin',
-    age: 26,
-    location: 'San Diego'
+    const option = e.target.elements.option.value;
+    
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+
 };
 
-function getLocation(location) {
-    if (location)
-        return <p> Location: {location} </p>; 
-}
+const removeOptions = () => {
+    app.options = [];
+    render();
+};
 
-var templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        <p>Age: {user.age}</p>
-        {getLocation(user.location)}
-    </div>
-);
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            {app.options.length > 0 ? <p>Here are your options</p> : 'No options'}
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button disabled={app.options.length === 0} onClick={removeOptions}>Remove Options</button>
+            <ol>
+                {
+                    app.options.map((option) => <li key={option}>Option: {option}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+const appRoot = document.getElementById('app');
+render();
 
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
