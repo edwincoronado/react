3e5ae948217b48d2eddf9bcb1bb9bdf3b93be9f9
edwin.coronado <edwin.coronado@servicenow.com>
2017-12-14 00:26:07 -0800

    Adding the momentum-mock

---
+++ momentum-mock/time-centered.js
@@ -0,0 +1,27 @@
+class TimeC extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.refreshTime = this.refreshTime.bind(this);
+        this.componentDidMount = this.refreshEverySecond.bind(this);
+        this.state = {
+            time: undefined,
+        };
+    }
+    refreshEverySecond() {
+        setInterval(() => {
+          this.setState({
+            time : new Date().toLocaleTimeString()
+          })
+        },1000)
+    }
+    render () {
+        return (
+            <div id="myTime">
+                <h2>Hello Edwin</h2>
+                <h1>{this.state.time}</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<TimeC />,  document.getElementById('app'));
